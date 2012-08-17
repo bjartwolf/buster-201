226f9cdee3a1ecb657d6f7cc29e98a9d3a08376e
Bjartwolf <bjartnes@gmail.com>
2012-08-17 13:16:34 +0200

    added one test

--- test/my-test.js
+++ test/my-test.js
@@ -1,8 +1,14 @@
 "use strict";
-buster.testCase("A module", {
-    "states the obvious": function () {
+buster.testCase("The ViewModel", {
+    "should update the fullname when firstname changes": function () {
         var vm = new AppViewModel();
         vm.firstName("Arne");
         assert.equals(vm.fullName(), "Arne Bertington");
+    },
+    "should capitalized the last name capitalizeLastName is called": function () {
+        var vm = new AppViewModel();
+        vm.firstName("Arne");
+        vm.capitalizeLastName();
+        assert.equals(vm.fullName(), "Arne BERTINGTON");
     }
 });
