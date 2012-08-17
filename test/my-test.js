3f00c8f3f087c9e57d32ea4367d4d8fc24f5c620
Bjartwolf <bjartnes@gmail.com>
2012-08-17 12:50:50 +0200

    init

--- test/my-test.js
+++ test/my-test.js
@@ -1,11 +1,8 @@
 "use strict";
-require("buster");
-var my = require("../lib/my.js");
 buster.testCase("A module", {
     "states the obvious": function () {
-	assert.equals(my.double(5), "10", "Should double the number");
-    },
-    "states the not-so-obvious": function () {
-	assert.same(my.double(5), "10", "Should double the number");
+	var vm = new AppViewModel();
+	vm.firstName("Arne");
+	assert.equals(vm.fullName(), "Arne Bertington");
     }
 });
